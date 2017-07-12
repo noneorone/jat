29263113438db15de1d4799fba3bcb81888e6869
Mars Wong <noneorone@yeah.net>
2017-07-12 10:46:40 +0800

    xie dian shen me dong xi

--- src/org/noneorone/data/json/FormatUtils.java
+++ src/org/noneorone/data/json/FormatUtils.java
@@ -2,8 +2,8 @@ package org.noneorone.data.json;
 
 /**
  * 功能说明：格式化工具<br/>
- * 作者：wangmeng on 2017/4/11 11:36<br/>
- * 邮箱：wangmeng@pycredit.cn
+ * 作者：Mars.Wong on 2017/4/11 11:36<br/>
+ * 邮箱：noneorone@yeah.net
  */
 
 public class FormatUtils {
