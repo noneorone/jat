ddeae0ad21327c7b4d0c8f349b99bd8107710244
mars.wong <noneorone@yeah.net>
2017-10-18 20:37:13 +0800

    zxing qrcode parse test

--- src/main/java/org/noneorone/image/QRCode.java
+++ src/main/java/org/noneorone/image/QRCode.java
@@ -11,54 +11,115 @@ import java.util.Map;
 import javax.imageio.ImageIO;
 
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.Binarizer;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.DecodeHintType;
 import com.google.zxing.EncodeHintType;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
 import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
 import com.google.zxing.qrcode.QRCodeWriter;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 public class QRCode {
 
-	public static void main(String[] args) throws IOException, WriterException {
+	public static void main(String[] args) throws Exception {
 
 		// File file = QRCode.from("Hello World").file();
 		// System.out.println(file.getPath());
-		File file = new File("c:\\qrcode.jpg");
-		if (file.exists()) {
-			file.delete();
+		// createQRImage("c:\\qrcode.jpg", "javascript:alert(222);", 200,
+		// "JPEG");
+		parseQRImage("c:\\qrcode.jpg");
+//		parseQRImage("c:\\qrcode_parser.png");
+	}
+
+	/**
+	 * Create QRCode Image.
+	 * 
+	 * @param qrFile
+	 * @param qrCodeText
+	 * @param size
+	 * @param fileType
+	 * @throws WriterException
+	 * @throws IOException
+	 */
+	public static void createQRImage(String path, String qrCodeText, int size, String fileType) {
+		try {
+			File file = new File(path);
+			if (file.exists()) {
+				file.delete();
+			}
+			if (!file.createNewFile()) {
+				return;
+			}
+
+			// Create the ByteMatrix for the QR-Code that encodes the given
+			// String
+			Map<EncodeHintType, Object> hintMap = new HashMap<EncodeHintType, Object>();
+			hintMap.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+			hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+			hintMap.put(EncodeHintType.MARGIN, 2);
+			QRCodeWriter qrCodeWriter = new QRCodeWriter();
+			BitMatrix byteMatrix = qrCodeWriter.encode(qrCodeText, BarcodeFormat.QR_CODE, size, size, hintMap);
+			// Make the BufferedImage that are to hold the QRCode
+			int matrixWidth = byteMatrix.getWidth();
+			BufferedImage image = new BufferedImage(matrixWidth, matrixWidth, BufferedImage.TYPE_INT_RGB);
+			image.createGraphics();
+
+			Graphics2D graphics = (Graphics2D) image.getGraphics();
+			graphics.setColor(Color.WHITE);
+			graphics.fillRect(0, 0, matrixWidth, matrixWidth);
+			// Paint and save the image using the ByteMatrix
+			graphics.setColor(Color.BLACK);
+
+			for (int i = 0; i < matrixWidth; i++) {
+				for (int j = 0; j < matrixWidth; j++) {
+					if (byteMatrix.get(i, j)) {
+						graphics.fillRect(i, j, 1, 1);
+					}
+				}
+			}
+			ImageIO.write(image, fileType, file);
+		} catch (WriterException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		file.createNewFile();
-		createQRImage(file, "javascript:alert(222);", 200, "JPEG");
 
 	}
 
-	private static void createQRImage(File qrFile, String qrCodeText, int size, String fileType) throws WriterException, IOException {
-		// Create the ByteMatrix for the QR-Code that encodes the given String
-		Map<EncodeHintType, Object> hintMap = new HashMap<EncodeHintType, Object>();
-		hintMap.put(EncodeHintType.CHARACTER_SET, "UTF-8");
-		hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
-		hintMap.put(EncodeHintType.MARGIN, 2);
-		QRCodeWriter qrCodeWriter = new QRCodeWriter();
-		BitMatrix byteMatrix = qrCodeWriter.encode(qrCodeText, BarcodeFormat.QR_CODE, size, size, hintMap);
-		// Make the BufferedImage that are to hold the QRCode
-		int matrixWidth = byteMatrix.getWidth();
-		BufferedImage image = new BufferedImage(matrixWidth, matrixWidth, BufferedImage.TYPE_INT_RGB);
-		image.createGraphics();
-
-		Graphics2D graphics = (Graphics2D) image.getGraphics();
-		graphics.setColor(Color.WHITE);
-		graphics.fillRect(0, 0, matrixWidth, matrixWidth);
-		// Paint and save the image using the ByteMatrix
-		graphics.setColor(Color.BLACK);
-
-		for (int i = 0; i < matrixWidth; i++) {
-			for (int j = 0; j < matrixWidth; j++) {
-				if (byteMatrix.get(i, j)) {
-					graphics.fillRect(i, j, 1, 1);
-				}
+	/**
+	 * 解析二维码图片
+	 * 
+	 * @param path
+	 */
+	public static void parseQRImage(String path) {
+		try {
+			File file = new File(path);
+			if (!file.exists()) {
+				return;
 			}
+
+			BufferedImage image = ImageIO.read(file);
+			LuminanceSource source = new BufferedImageLuminanceSource(image);
+			Binarizer binarizer = new HybridBinarizer(source);
+			BinaryBitmap bitmap = new BinaryBitmap(binarizer);
+			Map<DecodeHintType, Object> hints = new HashMap<>();
+			hints.put(DecodeHintType.CHARACTER_SET, "UTF-8");
+			hints.put(DecodeHintType.TRY_HARDER, Boolean.TRUE);
+			hints.put(DecodeHintType.PURE_BARCODE, Boolean.TRUE);
+			Result result = new MultiFormatReader().decode(bitmap, hints);
+			System.out.println(result);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (NotFoundException e) {
+			e.printStackTrace();
 		}
-		ImageIO.write(image, fileType, qrFile);
 	}
 
 }
